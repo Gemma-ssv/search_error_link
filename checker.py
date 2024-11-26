0ae818032ac55388f0a504f56a030dbc48a35b58
Gemma-ssv <eldarbolshov@gmail.com>
2024-11-26 09:17:19 +0300

    add documentation

--- checker.py
+++ checker.py
@@ -1,13 +1,47 @@
+"""Модуль для проверки ссылок на веб-страницах.
+
+Этот модуль содержит класс `LinkChecker`, который использует Selenium WebDriver для автоматизации браузера
+и проверки доступности ссылок на указанных веб-страницах. Результаты проверки сохраняются в Excel файл.
+
+Основные функции:
+- Проверка ссылок на новостях на указанных URL-адресах.
+- Сохранение информации о неработающих ссылках в Excel файл.
+
+Пример использования:
+```python
+urls = ['https://examle.by/']
+checker = LinkChecker(urls)
+checker.check_links()
+```
+"""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 import requests
+
 from utils import save_data
 
 
 class LinkChecker:
+    """
+    Класс для проверки ссылок на веб-страницах.
+
+    Атрибуты:
+    urls (list): Список URL-адресов для проверки.
+    data_to_save (list): Список для хранения данных о неработающих ссылках.
+    options (webdriver.ChromeOptions): Опции для настройки Chrome WebDriver.
+    """
+
     def __init__(self, urls):
+        """
+        Инициализация объекта LinkChecker.
+
+        Аргументы:
+        urls (list): Список URL-адресов для проверки.
+        """
         self.urls = urls
         self.data_to_save = []
         self.options = webdriver.ChromeOptions()
@@ -15,6 +49,14 @@ class LinkChecker:
         self.options.add_argument('--headless=new')
 
     def check_links(self):
+        """
+        Основной метод для проверки ссылок на указанных URL-адресах.
+
+        Описание:
+        - Инициализирует Chrome WebDriver.
+        - Проходит по каждому URL-адресу и вызывает метод _process_page для обработки страницы.
+        - Сохраняет данные о неработающих ссылках в Excel файл.
+        """
         try:
             with webdriver.Chrome(options=self.options) as browser:
                 for url in self.urls:
@@ -27,10 +69,22 @@ class LinkChecker:
                         save_data(self.data_to_save, url)
                     else:
                         print("Нет данных для сохранения.")
+        # pylint: disable=broad-exception-caught
         except Exception as e:
             print(f"Произошла ошибка: {e}")
 
     def _process_page(self, browser):
+        """
+        Обрабатывает текущую страницу, проверяя ссылки на новостях.
+
+        Аргументы:
+        browser (webdriver.Chrome): Экземпляр Chrome WebDriver.
+
+        Описание:
+        - Находит все ссылки на новости на текущей странице.
+        - Для каждой новости вызывает метод _process_news для проверки ссылок внутри новости.
+        - Проверяет наличие следующей страницы и переходит на нее, если она существует.
+        """
         while True:
             news_list = WebDriverWait(browser, 10).until(
                 EC.presence_of_all_elements_located((By.XPATH, "//div[@class='image']/a"))
@@ -38,6 +92,7 @@ class LinkChecker:
             for n_l in news_list:
                 try:
                     self._process_news(browser, n_l)
+                # pylint: disable=broad-exception-caught
                 except Exception as e2:
                     print(f"Ошибка при обработке новости: {e2}")
                     print(n_l.text)
@@ -55,11 +110,24 @@ class LinkChecker:
                 else:
                     print("Следующая страница не найдена, переход к следующей ссылке")
                     break
+            # pylint: disable=broad-exception-caught
             except Exception:
                 print("Конец парсинга.")
                 break
 
     def _process_news(self, browser, news_link):
+        """
+        Обрабатывает отдельную новость, проверяя ссылки внутри нее.
+
+        Аргументы:
+        browser (webdriver.Chrome): Экземпляр Chrome WebDriver.
+        news_link (WebElement): Элемент ссылки на новость.
+
+        Описание:
+        - Открывает ссылку на новость в новой вкладке.
+        - Находит все ссылки внутри новости и проверяет их доступность.
+        - Сохраняет информацию о неработающих ссылках.
+        """
         href = news_link.get_attribute('href')
         browser.execute_script(f"window.open('{href}', '_blank');")
         window_handles = browser.window_handles
@@ -94,6 +162,7 @@ class LinkChecker:
                           f"Неработающая ссылка: {href_checklink}")
                 else:
                     print('Элемент не найден - Всё хорошо')
+            # pylint: disable=broad-exception-caught
             except Exception as e:
                 self.data_to_save.append({
                     "Основная статья": h1_link_list,
--- main.py
+++ main.py
@@ -1,119 +1,32 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import requests
-from utils import get_time_script, save_data
+"""
+Основной модуль для запуска проверки ссылок на веб-страницах.
+
+Этот модуль содержит функцию `main`, которая 
+инициализирует объект `LinkChecker` и запускает процесс проверки ссылок.
+Проверка выполняется для указанных URL-адресов, и результаты сохраняются в Excel файл.
+
+Пример использования:
+```python
+python main.py
+```
+"""
+from checker import LinkChecker
+from utils import get_time_script
 
 @get_time_script
 def main():
-    # Список для хранения данных
-    data_to_save = []
-    # start_time = time.time()
-    options = webdriver.ChromeOptions()
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument('--headless=new')
+    """
+    Основная функция для запуска проверки ссылок.
 
-    # Инициализация драйвера
-    try:
-        with webdriver.Chrome(options=options) as browser:
-            urls = ['https://gemma.by/soveti/']
-            for url in urls:
-                data_to_save = []  # Очищаем список данных для каждой новой страницы
-                browser.get(url)
-                
-                while True:
-                    news_list = browser.find_elements(By.XPATH, "//div[@class='image']/a")  # Ссылки новостей
-                    for n_l in news_list:
-                        try:
-                            first_link = n_l
-                            
-                            # Получить значение атрибута href
-                            href = first_link.get_attribute('href')
-                            
-                            # Открыть ссылку в новой вкладке с помощью JavaScript
-                            browser.execute_script(f"window.open('{href}', '_blank');")
-                            
-                            # Получить список всех открытых вкладок
-                            window_handles = browser.window_handles
-                            # Переключиться на новую вкладку
-                            browser.switch_to.window(window_handles[-1])
-                            link_list = browser.find_elements(By.TAG_NAME, "article")  # Основной блок где ищем ссылки на странице новости
-                            link_list = link_list[0].find_elements(By.TAG_NAME, "a") # Выбираем ссылки на странице новостей
-                            
-                            h1_link_list = browser.find_element(By.CLASS_NAME, "rt1")  # Название статьи
-                            h1_link_list = h1_link_list.text
-                            
-                            if link_list:
-                                for l_l in link_list:
-                                    l_l_text = l_l.text
-                                    href_checklink = l_l.get_attribute('href')  # Ссылка для проверки в самой новости
+    Описание:
+    - Инициализирует объект `LinkChecker` с указанными URL-адресами.
+    - Запускает процесс проверки ссылок.
+    - Результаты проверки сохраняются в Excel файл.
+    """
 
-                                    try:
-                                        response = requests.get(href_checklink)    
-                                        # Получить список всех открытых вкладок
-                                        window_handles = browser.window_handles
-                                        
-                                        time.sleep(1)
-                                        if response.status_code != 200:
-                                            data_to_save.append({
-                                                    "Основная статья": h1_link_list,
-                                                    "Ссылка на основную статью": href,
-                                                    "Ошибка": response.status_code,
-                                                    "Текст ссылки в основной статье": l_l_text,
-                                                    "Неработающая ссылка": href_checklink
-                                                })
-                                            print(f"Основная статья: {h1_link_list},\n"
-                                                    f"Ссылка на основную статью: {href},\n"
-                                                    f"Ошибка: {response.status_code},\n" 
-                                                    f"Текст ссылки в основной статье: {l_l_text},\n"
-                                                    f"Неработающая ссылка: {href_checklink}")
-                                        else:
-                                            print('Элемент не найден - Всё хорошо')
-
-                                        time.sleep(1)  # Пример задержки для демонстрации
-                                    except Exception as e:
-                                        data_to_save.append({
-                                                    "Основная статья": h1_link_list,
-                                                    "Ссылка на основную статью": href,
-                                                    "Ошибка": "Некорректная ссылка",
-                                                    "Текст ссылки в основной статье": l_l_text,
-                                                    "Неработающая ссылка": href_checklink
-                                                })
-                                        print(e)
-                            else:
-                                print("Ссылки на странице новостей не найдены")
-                        except Exception as e2:
-                            print(f"Ошибка при обработке новости: {e2}")
-                            print(n_l.text)
-                            continue
-                        
-                        # Закрыть вкладку с новостью и вернуться на главную страницу
-                        browser.close()
-                        browser.switch_to.window(window_handles[0])
-                    
-                    # Проверка наличия следующей страницы
-                    try:
-                        str_list = browser.find_element(By.CLASS_NAME, "pagination")
-                        str_list = str_list.find_elements(By.TAG_NAME, "a")
-                        next_page_link = str_list[-2]  # последняя ссылка в списке - это следующая страница
-                        if ">" in next_page_link.text:
-                            next_page_link.click()
-                            time.sleep(2)  # Дать время для загрузки следующей страницы
-                        else:
-                            print("Следующая страница не найдена, переход к следующей ссылке")
-                            break
-                    except Exception:
-                        print("Конец парсинга.")
-                        break
-
-                # Создание Excel файла и запись данных для текущей страницы
-                if data_to_save:
-                    save_data(data_to_save, url)
-
-                else:
-                    print("Нет данных для сохранения.")
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
+    urls = ['https://gemma.by/soveti/']
+    checker = LinkChecker(urls)
+    checker.check_links()
 
 if __name__ == "__main__":
     main()
