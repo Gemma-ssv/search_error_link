27b9b0fc5e03767ebc3ba0f4d1090dc0c4950b27
Gemma-ssv <eldarbolshov@gmail.com>
2024-11-26 08:42:43 +0300

    create function clean filename, save_data

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import time
-
+import openpyxl
+import re
 
 def get_time_script(fun):
     def wrapper(*args, **kwargs):
@@ -14,3 +15,28 @@ def get_time_script(fun):
         print(f"Время выполнения скрипта: {int(minutes)} минут {int(seconds)} секунд")
         return result
     return wrapper
+
+def clean_filename(url):
+    # Удаляем все символы, которые не являются буквами, цифрами или подчеркиванием
+    cleaned_url = re.sub(r'[^a-zA-Z0-9_]', '_', url)
+    return cleaned_url
+
+def save_data(data_to_save, url):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = "Error"
+                    
+    # Заголовки столбцов
+    headers = ["Основная статья", "Ссылка на основную статью", "Ошибка", "Текст ссылки в основной статье", "Неработающая ссылка"]
+    sheet.append(headers)
+                    
+    # Запись данных
+    for data in data_to_save:
+        sheet.append([data["Основная статья"], data["Ссылка на основную статью"], data["Ошибка"], data["Текст ссылки в основной статье"], data["Неработающая ссылка"]])
+    cleaned_url = clean_filename(url)
+    # Определение имени файла в зависимости от URL
+    filename = f"error{cleaned_url}.xlsx"
+                   
+    # Сохранение файла
+    workbook.save(filename)
+    print(f"Данные успешно сохранены в файл {filename}")
