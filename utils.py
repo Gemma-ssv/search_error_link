955e50473fa6a36baaf652c9e76c7f9e8484e90f
Gemma-ssv <eldarbolshov@gmail.com>
2024-11-27 15:43:55 +0300

    used set not list

--- utils.py
+++ utils.py
@@ -29,7 +29,6 @@ import time
 import threading
 import re
 import openpyxl
-import validators
 
 
 # Добавляем блокировку для синхронизации вывода в консоль
@@ -164,57 +163,54 @@ def is_valid_url(url_input):
     Возвращает:
         bool: Правильный формат или неправильный формат ссылки.
     """
-    return validators.url(url_input)
+    pattern = r'^https?:\/\/[^\/]+\/[^\s]*\/$'
+    return re.match(pattern, url_input) is not None
 
-def print_choice(urls: list) -> list:
+def print_choice(urls: set) -> set:
     """
     Выводит информацию о программе. Запрашивает у пользователя ссылки.
 
     Аргументы:
-        urls (list): Получает пустой список, в который будут записаны ссылки.
+        urls (set): Получает пустой список, в который будут записаны ссылки.
 
     Возвращает:
-        list: Возвращает список ссылок, которые ввёл пользователь.
+        set: Возвращает список ссылок, которые ввёл пользователь.
     """
     example_text = (
+        "Программа предназначена для проверки ссылок в новостях в интернет-магазине.\n"
         "Введите ссылку в формате - https://домен/путь/\n"
         "Например - https://gemma.by/news/\n"
-        "Или например с параметром - https://gemma.by/news/?page=2\n"
-        "В любом из форматов программа будет искать все страницы навовостей."
         "После ввода нажмите - Enter.\n"
-        )
+    )
     print_slowly(example_text)
+    
     while True:
-        example_text = ("Введите ссылку")
+        example_text = "Введите ссылку: "
         print_slowly(example_text)
         url_input = input().strip()
 
         if is_valid_url(url_input):
-            urls.append(url_input)
+            urls.add(url_input)
             while True:
                 choice_text = ("Выполнить поиск? Введите `да` или `нет`.\n"
-                    "Если `нет`, то можно будет добавить еще ссылку.\n")
+                               "Если `нет`, то можно будет добавить еще ссылку.\n")
                 print_slowly(choice_text)
                 next_input = input().lower().strip()
 
                 if next_input == 'да':
-                    break
-                if next_input == 'нет':
+                    answer_yes_text = "Дождитесь окончания выполнения программы.\n"
+                    print_slowly(answer_yes_text)
+                    return urls
+                elif next_input == 'нет':
                     break
                 else:
-                    wrong_answer_text = "Неправильные ответ. Требуется ввести `да` или `нет`.\n"
+                    wrong_answer_text = "Неправильный ответ. Требуется ввести `да` или `нет`.\n"
                     print_slowly(wrong_answer_text)
                     continue
-
-            if next_input == 'да':
-                answer_yes_text = "Дождитесь окончания выполнения программы.\n"
-                print_slowly(answer_yes_text)
-                break
         else:
             wrong_link_text = "Вы ввели неправильный формат ссылки. Попробуйте еще раз.\n"
             print_slowly(wrong_link_text)
             continue
-    return urls
 
 def animate_search(stop_event):
     """
